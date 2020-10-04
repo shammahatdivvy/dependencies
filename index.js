09e80d691ff2521ae49e68a958888a6af7c929a0
Shammah Chancellor <shammah@divvyhomes.com>
2020-10-04 07:44:43 -0700

    Imports fully working, and top level declarations are detected

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const stringify = require("fast-safe-stringify");
 const path = require("path");
 const fs = require('fs');
 const { walk } = require('walk');
+const R = require('ramda');
+const { assert } = require("console");
 
 const babelOptions = {
     sourceType: "module",
     strictMode: false,
     allowImportExportEverywhere: true,
     allowReturnOutsideFunction: true,
+    allowAwaitOutsideFunction: true,
     startLine: 1,
     tokens: true,
+    errorRecovery: true,
     plugins: [
         "asyncGenerators",
         "bigInt",
@@ -36,96 +40,225 @@ const babelOptions = {
         ["pipelineOperator", { proposal: "minimal" }],
         "throwExpressions",
         "jsx",
-        "typescript"
+        "typescript",
+        "topLevelAwait"
     ]
 };
 
 const modules = {};
 
-function countModule(moduleName) {
-    const indexJsFile = moduleName + '/index.js'
-    const jsFile = moduleName + '/index.js'
-    if (fs.existsSync(indexJsFile)) {
-        moduleName = indexJs
-    } else if (fs.existsSync(jsFile)) {
-        moduleName += '.js'
+function countModule(moduleName, includingFile) {
+    const potentialFiles = [
+        moduleName + '.js',
+        moduleName + '/index.js',
+        moduleName + '/index.ts',
+        moduleName + '.ts',
+        moduleName + '.jsx',
+        moduleName + '.tsx',
+        path.join(moduleName, R.last(moduleName.split(path.sep)) + '.js'), // Weird search behavior
+        path.join(moduleName, R.last(moduleName.split(path.sep)) + '.jsx'), // Weird search behavior
+        moduleName,
+    ];
+    for (const fileName of potentialFiles) {
+        if (!fs.existsSync(fileName)) {
+            continue;
+        }
+        if (!(fileName in modules)) {
+            modules[fileName] = {};
+        }
+        modules[fileName][includingFile] = true;
+        return;
     }
+    // Just include the module name directly.
     if (!(moduleName in modules)) {
-        modules[moduleName] = 0;
+        modules[moduleName] = {};
     }
-    modules[moduleName]++;
+    modules[moduleName][includingFile] = true;
 }
 
-function normalizeModuleName(moduleName) {
-    if (path.extname(moduleName) === '') {
-        return path.join(moduleName, 'index.js')
-    }
-    return moduleName
-}
+const specialPaths = {
+    '@app': './',
+    '@client': './client',
+    '@shared': './',
+};
 
 function addModule(moduleName, sourcePath, rootPath) {
     const splitName = moduleName.split(path.sep);
     if (['.', '..'].includes(splitName[0])) {
-        // Remove source file name
-        splitName.pop();
-        const modulePath = path.join(...splitName);
-        countModule(path.join(sourcePath, normalizeModuleName(modulePath)));
-    } else if (['@app', '@shared', '@client'].includes(splitName[0])) {
+        const basePath = sourcePath.split(path.sep);
+        basePath.pop();
+        countModule(path.join(...basePath, moduleName), sourcePath);
+    } else if (specialPaths[splitName[0]]) {
         // Remove special path specifier
+        const prefixPath = specialPaths[splitName[0]];
         splitName.shift();
-        const modulePath = path.join(...splitName);
-        countModule(path.join(rootPath, normalizeModuleName(modulePath)))
+        const modulePath = path.join(prefixPath, ...splitName);
+        countModule(path.join(rootPath, modulePath), sourcePath);
     } else {
-        countModule(moduleName);
+        countModule(moduleName, sourcePath);
     }
+}
+
+const usedIdentifiers = {};
+const declaredIdentifiers = {};
+
+
+function addUsedIdentifier(identifier) {
+    // Just include the module name directly.
+    if (!(identifier in usedIdentifiers)) {
+        usedIdentifiers[identifier] = 0;
+    }
+    usedIdentifiers[identifier] += 1;
+}
+
+function addDeclaration(identifier, sourcePath) {
+    // Just include the module name directly.
+    if (!(identifier in declaredIdentifiers)) {
+        declaredIdentifiers[identifier] = [];
+    }
+    declaredIdentifiers[identifier].push(sourcePath);
+}
+
+function createCallExpressionVisitor(sourcePath, rootPath) {
+    return function (astPath) {
+        const value = astPath.value;
+
+        // Handle tracking require'd files
+        if (value.callee.name === 'require') {
+            const moduleName = value.arguments[0].value;
+            if (!moduleName) {
+                return this.traverse(astPath);
+            }
+            addModule(moduleName, sourcePath, rootPath);
+            return this.traverse(astPath);
+        }
+
+        // Handle tracking imported files
+        if (value.callee.type === 'Import') {
+            const moduleName = value.arguments[0].value;
+            if (!moduleName) {
+                return this.traverse(astPath);
+            }
+            addModule(moduleName, sourcePath, rootPath);
+            return this.traverse(astPath);
+        }
+
+        // Handle tracking other function calls.
+        const functionName = value.callee.name;
+        if (functionName) {
+            addUsedIdentifier(functionName);
+        }
+        this.traverse(astPath, {
+            visitMemberExpression(astPath) {
+                const objectName = astPath.value.object.name;
+                if (objectName) {
+                    addUsedIdentifier(objectName)
+                }
+                return false;
+            },
+            visitIdentifier(astPath) {
+                const value = astPath.value;
+                addUsedIdentifier(value.name)
+                return false;
+            },
+            visitCallExpression: createCallExpressionVisitor(sourcePath, rootPath),
+        })
+    };
 }
 
 function processSource(source, sourcePath, rootPath) {
     const ast = parse(source, babelOptions);
-
+    const visitCallExpression = createCallExpressionVisitor(sourcePath, rootPath);
     visit(ast, {
-        // This method will be called for any node with .type "MemberExpression":
-        // visitIdentifier(path) {
-        //     console.log(path.value.name);
-        //     return false;
-        //     this.traverse(path);
-        // },
-        visitCallExpression(astPath) {
+        visitFunctionDeclaration(astPath) {
             const value = astPath.value;
-            if (astPath.value.callee.name === 'require') {
-                const moduleName = value.arguments[0].value;
-                addModule(moduleName, sourcePath, rootPath)
+            // Some function declarations are anonymous.
+            if (value.id) {
+                const name = value.id.name;
+                addDeclaration(name, sourcePath);
             }
-            this.traverse(astPath);
+            // We only want to visit call expressions 
+            this.traverse(astPath, {
+                visitCallExpression, visitIdentifier(astPath) {
+                    const value = astPath.value;
+                    addUsedIdentifier(value.name)
+                    return false;
+                },
+            });
+        },
+        visitVariableDeclarator(astPath) {
+            const value = astPath.value;
+            const name = value.id.name;
+            // Filter out require definitions
+            if (name && value.init && !(value.init.type === 'CallExpression' && value.init.callee.name === 'require')) {
+                // some declarations are via object unpacking
+                // we don't really care much about these. 
+                addDeclaration(name, sourcePath);
+            }
+            this.traverse(astPath, {
+                visitCallExpression, visitIdentifier(astPath) {
+                    const value = astPath.value;
+                    addUsedIdentifier(value.name)
+                    return false;
+                },
+            });
         },
         visitImportDeclaration(astPath) {
             const moduleName = astPath.value.source.value;
             addModule(moduleName, sourcePath, rootPath);
-            this.traverse(astPath);
-        }
+            return false;
+        },
+        visitCallExpression
     });
 }
+const walkOptions = {
+    followLinks: false,
+    filters: [
+        "_test",
+        "node_modules",
+        "test",
+        "stories",
+        "build",
+        ".storybook",
+        "eslint-plugin-divvy-rules",
+        "cypress",
+        "coverage",
+        "jest",
+        "public",
+    ]
+}
 
-const walker = walk('../divvy-homes/src', { followLinks: false });
-
-walker.on("file", (root, fileStats, next) => {
+const indexingWalker = walk('../divvy-homes', walkOptions);
+indexingWalker.on("file", (root, fileStats, next) => {
     if (!['.js', '.jsx', '.ts', '.tsx'].includes(path.extname(fileStats.name))) {
         return next();
     }
-    if (fileStats.name.split(fs.sep).includes('_test')) {
-        return next();
-    }
-    const fullPath = path.join(root, fileStats.name);
-    fs.readFile(fullPath, function (err, data) {
-        // console.log("Processing", fullPath);
-        try {
-            processSource(data.toString(), fullPath, '../divvy-homes/src');
-        } catch (err) {
-            console.log(err);
-        }
-        next();
-    });
+    modules[path.join(root, fileStats.name)] = {};
+    next();
 }).on('end', () => {
-    console.log(stringify(modules));
+    const walker = walk('../divvy-homes', walkOptions);
+    walker.on("file", (root, fileStats, next) => {
+        if (!['.js', '.jsx', '.ts', '.tsx'].includes(path.extname(fileStats.name))) {
+            return next();
+        }
+        const fullPath = path.join(root, fileStats.name);
+        fs.readFile(fullPath, function (err, data) {
+            // console.log("Processing", fullPath);
+            try {
+                processSource(data.toString(), fullPath, '../divvy-homes/src');
+            } catch (err) {
+                console.log("error processing sourcefile:", fullPath)
+                console.log(err);
+            }
+            next();
+        });
+    }).on('end', () => {
+        for (const [key, value] of Object.entries(modules)) {
+            console.log(`${key}, ${Object.entries(value).length}`)
+        }
+        // for (const [key, value] of Object.entries(declaredIdentifiers)) {
+        //     console.log(`${key}, ${usedIdentifiers[key]}, ${value}`)
+        // }
+    })
 })
 
